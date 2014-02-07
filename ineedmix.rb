e72f228878bf0b81152bb78969d9dc580ca39c2d
Nat Welch <nat@natwelch.com>
2014-02-07 15:49:26 -0800

    internet too slow.

--- ineedmix.rb
+++ ineedmix.rb
@@ -1,6 +1,7 @@
 #! /usr/bin/env ruby
 
 require 'fileutils'
+require 'optparse'
 
 # https://github.com/dinkypumpkin/get_iplayer
 GET_IPLAYER = "~/Projects/get_iplayer/get_iplayer"
@@ -15,23 +16,21 @@ FileUtils.mkdir_p(File.dirname(File.expand_path(DOWNLOAD_PATH)))
 
 SHOWS = ["Pete Tong", "Annie Mac", "BBC Radio 1's Essential Mix"]
 
-USAGE = <<END_OF_USAGE
-  
-  Usage: #{__FILE__} [refresh|download|find|get] (find needs the name) (get needs the PID)"
-  
-  *********** MANUAL REFRESH
-  #{GET_IPLAYER} --refresh --type=radio
-  
-  *********** MANUAL FIND
-  cat ~/.get_iplayer/radio.cache | grep -i '[SHOW NAME]' | awk 'BEGIN {FS=\"|\"}{ print $4, \" \",  $3, \" \", $11 }'
-  
-  *********** MANUAL GET
-  #{GET_IPLAYER} --type=radio --pid [PID] --output #{DOWNLOAD_PATH}
-END_OF_USAGE
 
-if ARGV.length == 0
-  puts USAGE
-  exit
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("--type [TYPE]", [:text, :binary, :auto],
+          "Select transfer type (text, binary, auto)") do |t|
+    options.transfer_type = t
+          end
+
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
 end
 
 if ARGV[0] == "refresh"
@@ -53,7 +52,7 @@ elsif ARGV[0] == "download"
         downloaded = `cat #{IPLAYER_HISTORY} | grep "#{parts[3]}"`  
         if downloaded.empty?
           puts "Download   #{parts[3]} - #{parts[2]} - #{parts[10]}"
-          cmd = "#{GET_IPLAYER} --modes=flashaaclow,rtspaaclow,wma --type=radio --pid #{parts[3]} --output #{DOWNLOAD_PATH}"
+          cmd = "#{GET_IPLAYER} --modes=best --aactomp3 --type=radio --pid #{parts[3]} --output #{DOWNLOAD_PATH}"
           puts cmd
           p Kernel.system cmd
         else
