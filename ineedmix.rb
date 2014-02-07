410c696bec0171f77281c29de6ced813233af8ec
Nat Welch <nat@natwelch.com>
2014-02-07 15:17:09 -0800

    debugging

--- ineedmix.rb
+++ ineedmix.rb
@@ -1,10 +1,18 @@
 #! /usr/bin/env ruby
 
+require 'fileutils'
+
 # https://github.com/dinkypumpkin/get_iplayer
 GET_IPLAYER = "~/Projects/get_iplayer/get_iplayer"
 IPLAYER_CACHE = "~/.get_iplayer/radio.cache"
 IPLAYER_HISTORY = "~/.get_iplayer/download_history"
 DOWNLOAD_PATH = "~/Projects/snowball/public/misc/"
+
+FileUtils.mkdir_p(File.dirname(File.expand_path(IPLAYER_CACHE)))
+FileUtils.touch(File.expand_path(IPLAYER_CACHE))
+FileUtils.mkdir_p(File.dirname(File.expand_path(IPLAYER_HISTORY)))
+FileUtils.touch(File.expand_path(IPLAYER_HISTORY))
+FileUtils.mkdir_p(File.dirname(File.expand_path(DOWNLOAD_PATH)))
 
 SHOWS = ["Pete Tong", "Annie Mac", "BBC Radio 1's Essential Mix"]
 
@@ -47,11 +55,9 @@ elsif ARGV[0] == "download"
         downloaded = `cat #{IPLAYER_HISTORY} | grep "#{parts[3]}"`  
         if downloaded.empty?
           puts "Download   #{parts[3]} - #{parts[2]} - #{parts[10]}"
-          if parts[3] =~/^p/
-            `#{GET_IPLAYER} --type=radio --pid #{parts[3]} --output #{DOWNLOAD_PATH}`
-          else
-            `#{GET_IPLAYER} --type=radio --pid #{parts[3]} --output #{DOWNLOAD_PATH}`
-          end  
+          cmd = "#{GET_IPLAYER} --modes=flashaaclow,rtspaaclow,wma --type=radio --pid #{parts[3]} --output #{DOWNLOAD_PATH}"
+          puts cmd
+          p Kernel.system cmd
         else
           puts "Downloaded #{parts[3]} - #{parts[2]} - #{parts[10]}"
         end  
@@ -84,11 +90,7 @@ elsif ARGV[0] == "get" && ARGV.length > 1
   downloaded = `cat #{IPLAYER_HISTORY} | grep "#{ARGV[1]}"`  
   if downloaded.empty?
     puts "Downloading #{ARGV[1]}"
-    if ARGV[1] =~/^p/
-      `#{GET_IPLAYER} --type=radio --pid #{ARGV[1]} --output #{DOWNLOAD_PATH}`
-    else
-      `#{GET_IPLAYER} --type=radio --pid #{ARGV[1]} --output #{DOWNLOAD_PATH}`
-    end  
+    `#{GET_IPLAYER} --type=radio --pid #{ARGV[1]} --output #{DOWNLOAD_PATH}`
   else
     puts "Downloaded  #{ARGV[1]}"
   end  
