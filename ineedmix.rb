0b794e728e13afd1cba7eb3f6087c6ef5bed464c
Nat Welch <nat@natwelch.com>
2014-02-07 16:04:57 -0800

    Better option parsing

--- ineedmix.rb
+++ ineedmix.rb
@@ -7,60 +7,85 @@ require 'optparse'
 GET_IPLAYER = "~/Projects/get_iplayer/get_iplayer"
 IPLAYER_CACHE = "~/.get_iplayer/radio.cache"
 IPLAYER_HISTORY = "~/.get_iplayer/download_history"
-DOWNLOAD_PATH = "~/Projects/snowball/public/misc/"
 
 FileUtils.mkdir_p(File.dirname(File.expand_path(IPLAYER_CACHE)))
 FileUtils.mkdir_p(File.dirname(File.expand_path(IPLAYER_HISTORY)))
 FileUtils.touch(File.expand_path(IPLAYER_HISTORY))
-FileUtils.mkdir_p(File.dirname(File.expand_path(DOWNLOAD_PATH)))
 
-SHOWS = ["Pete Tong", "Annie Mac", "BBC Radio 1's Essential Mix"]
+options = {
+  :action => nil,
+  :directory => "~/Projects/snowball/public/misc/",
+  :shows => ["Pete Tong", "Annie Mac", "BBC Radio 1's Essential Mix"],
+}
 
-
-
-options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: example.rb [options]"
+  opts.banner = "Usage: #{__FILE__} [options]"
 
-  opts.on("--type [TYPE]", [:text, :binary, :auto],
-          "Select transfer type (text, binary, auto)") do |t|
-    options.transfer_type = t
-          end
+  opts.on("-a", "--action [ACTION]", [:refresh, :download], "Select action to perform: refresh or download.") do |t|
+     options[:action] = t
+  end
+
+  opts.on("-d", "--directory [DOWNLOAD_PATH]", "Directory to Download to.") do |d|
+    options[:directory] = d
+  end
+
+  opts.on("-s", "--shows x,y,z", Array, "List of shows to try and download.") do |list|
+    options[:shows] = list
+  end
 
   opts.on_tail("-h", "--help", "Show this message") do
     puts opts
     exit
   end
-end
+end.parse!
 
-if ARGV[0] == "refresh"
-  puts "#{'*'*80}"
-  puts "*********** REFRESH (can take some time)"
-  
-  Kernel.system "#{GET_IPLAYER} --refresh --type=radio"
-elsif ARGV[0] == "download"
-  puts "#{'*'*80}"
-  puts "*********** download"
-  
-  SHOWS.each do |name|
-    shows = `cat #{IPLAYER_CACHE} | grep -i "#{name}"`
-    unless shows.empty?
-      shows.split("\n").each do |show|
-        puts "#{'*'*80}"
+class INeedMix
+  def initialize options
+    @shows = options[:shows]
+    @directory = options[:directory]
+    @action = options[:action]
 
-        parts = show.split('|')
-        downloaded = `cat #{IPLAYER_HISTORY} | grep "#{parts[3]}"`  
-        if downloaded.empty?
-          puts "Download   #{parts[3]} - #{parts[2]} - #{parts[10]}"
-          cmd = "#{GET_IPLAYER} --modes=best --aactomp3 --type=radio --pid #{parts[3]} --output #{DOWNLOAD_PATH}"
-          puts cmd
-          p Kernel.system cmd
-        else
-          puts "Downloaded #{parts[3]} - #{parts[2]} - #{parts[10]}"
-        end  
-      end  
-    end  
-  end  
-else
-  puts USAGE
+    FileUtils.mkdir_p(File.dirname(File.expand_path(@directory)))
+  end
+
+  def do_work
+    if @action == :refresh
+      self.refresh
+    elsif @action == :download
+      self.download
+    else
+      puts "I have not idea what \"#{@action}\" is."
+      exit
+    end
+  end
+
+  def refresh
+    puts "*********** REFRESH (can take some time)"
+    Kernel.system "#{GET_IPLAYER} --refresh --type=radio"
+  end
+
+  def download
+    puts "#{'*'*80}"
+    puts "*********** download"
+
+    @shows.each do |name|
+      shows = `cat #{IPLAYER_CACHE} | grep -i "#{name}"`
+      unless shows.empty?
+        shows.split("\n").each do |show|
+          puts "#{'*'*80}"
+
+          parts = show.split('|')
+          downloaded = `cat #{IPLAYER_HISTORY} | grep "#{parts[3]}"`
+          if downloaded.empty?
+            puts "Download   #{parts[3]} - #{parts[2]} - #{parts[10]}"
+            cmd = "#{GET_IPLAYER} --modes=best --aactomp3 --type=radio --pid #{parts[3]} --output #{@directory}"
+            puts cmd
+            p Kernel.system cmd
+          else
+            puts "Downloaded #{parts[3]} - #{parts[2]} - #{parts[10]}"
+          end
+        end
+      end
+    end
+  end
 end
