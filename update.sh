86716a1ab3c96138e86e28597deecf70d244f498
Nat Welch <nat@natwelch.com>
2016-01-16 11:02:38 -0800

    add update script

---
+++ update.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+RUBY=~/.rvm/environments/ruby-2.3.0
+
+if [[ ! -f $RUBY ]] ; then
+  echo "File $RUBY is not there, aborting."
+  exit
+fi
+
+source $RUBY
+
+git pull
+bundle update
+git ci Gemfile* -m 'bundle update'
+git push
+
+./ineedmix.rb -i ../get_iplayer/get_iplayer -a refresh; ./ineedmix.rb -i ../get_iplayer/get_iplayer -d /tmp/ -a download
